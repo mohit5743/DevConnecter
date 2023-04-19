f8c4dc5165300be7f91b5b8dc8004b41a0b3f4dc
mohit chopra <mohitchopra5743@gmail.com>
2023-04-19 19:03:27 +0530

    init

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema= new mongooseSchema({
+    name :{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports= User = mongoose.model('user','UserSchema');
