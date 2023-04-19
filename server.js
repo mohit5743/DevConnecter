3cd0e3316123bac34564e961c75c2832e34ab09f
mohit chopra <mohitchopra5743@gmail.com>
2023-04-19 18:51:02 +0530

    init

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ connectDB();
 
 app.get('/',(req,res)=> res.send("API Running"));
 
+app.use('/api/user', require('./routers/api/users'));
+app.use('/api/auth',require('./routers/api/auth'));
+app.use('/api/posts',require('./routers/api/posts'));
+app.use('/api/profile',require('./routers/api/profile'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log("Server started on port ${PORT}"));
