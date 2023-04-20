4214e5bddbd2a4c92a6665228d412b2889118a1a
mohit chopra <mohitchopra5743@gmail.com>
2023-04-20 15:51:43 +0530

    init

--- models/User.js
+++ models/User.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
-const UserSchema= new mongooseSchema({
+const UserSchema= new mongoose.Schema({
     name :{
         type: String,
         required: true
     },
     email:{
         type: String,
-        required: true
+        required: true,
+        unique : true
 
     },
     password:{
         type: String,
         require: true
+    },
+    avatar:{
+        type: String
     },
     date:{
         type:Date,
