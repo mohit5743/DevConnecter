fa4f29f62ea3755be4ebd29b23eb47f88a591d98
mohit chopra <mohitchopra5743@gmail.com>
2023-04-21 11:05:21 +0530

    init

--- routers/api/auth.js
+++ routers/api/auth.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); 
+const  User= require('../../models/User'); 
+router.get('/',auth, async (req,res)=>
+    {
+        try{
+            const user = await  User.findById(req.user.id).select('-password');
+            res.json(user);
 
-router.get('/',(req,res)=>
-    res.send("autor route")
+        }catch(err){
+            console.error(err.message);
+            re.status(500).send('Server Error ');
+
+        }
+    }
 );
-module.exports=router;
+module.exports=router;  
--- routers/api/users.js
+++ routers/api/users.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const{  check, validationResult} = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt=require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const  config= require('config');
 const User = require('../../models/User');
+router.get('/',(req,res)=>
+    res.send("user route")
+);
 
 router.post('/' ,[
     check('name','Name is required')
@@ -38,7 +43,20 @@ router.post('/' ,[
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password,salt);
     await user.save();
-    res.send("user registered")}
+    const payload={
+        user:{
+            id: user.id
+        }
+
+    }
+    jwt.sign(payload,
+        config.get('jwtsecret'),
+        {expiresIn: 360000},
+        (err,token)=>{
+            if(err) throw err;
+            res.json({token});
+        }  )
+}
     catch(err){
         console.error(err.message);
         res.status(500).send('Server error');
